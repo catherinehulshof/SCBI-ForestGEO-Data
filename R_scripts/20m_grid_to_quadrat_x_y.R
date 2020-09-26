4bc73e6e3d0b30a1cdb9127a98a6098227e9928d
Catherine Hulshof <catherine.hulshof@gmail.com>
2020-09-26 09:00:19 -0400

    Extrat quadrat xy coords from 20m_grid shp file

---
+++ R_scripts/20m_grid_to_quadrat_x_y.R
@@ -0,0 +1,12 @@
+# accessing spatial data
+
+library(sf); library(fgeo); library(tidyverse)
+
+plot <- read.csv('tree_main_census/data/census-csv-files/scbi.stem3.csv')
+serc <- st_read('spatial_data/shapefiles/20m_grid.shp')
+serc.df <- as.data.frame(serc)
+
+serc.df.short <- serc.df %>% 
+  select(PLOT,STAKE_X, STAKE_Y) 
+names(serc.df.short) <-  c('quadrat','x','y')
+write.csv(serc.df.short, 'quadrat.coords.csv',quote=F)
